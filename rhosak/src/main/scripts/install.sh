f95127b9eb71c75e7c47dbb77584bc4d4de64ca8
Kabir Khan <kkhan@redhat.com>
2023-02-16 17:49:18 +0100

    Add RHOSAK example code

---
+++ rhosak/src/main/scripts/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This script (install.sh) will be executed during build 'server-build-artifacts'
+# This script copies the CLI scripts to $JBOSS_HOME/extensions to be processed in the post hook
+# The post hook part means that on launching of the application server the postconfigure.sh is executed
+
+set -x
+echo "Running microprofile-reactive-messaging-kafka/install.sh"
+injected_dir=$1
+echo "Copying "$1" directory to $JBOSS_HOME/extensions"
+cp -rf ${injected_dir} $JBOSS_HOME/extensions
